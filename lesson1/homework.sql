b8e42ce25366fd84eb43642f6cc23164c42fc0cc
crasher307 <crasher307@gmail.com>
2023-05-27 05:46:01 +0500

    add lesson 1

---
+++ lesson1/homework.sql
@@ -0,0 +1,44 @@
+# 1
+create database if not exists gb_1;
+use gb_1;
+drop table if exists phones;
+create table phones (
+    `id` int(16) primary key auto_increment,
+    `phone_name` varchar(32) comment 'Модель',
+    `manufacturer` varchar(32) comment 'Производитель',
+    `product_count` int(8) comment 'Кол-во',
+    `price` int(8) comment 'Цена (шт)'
+) auto_increment = 0;
+insert into phones (phone_name, manufacturer, product_count, price)
+values ('iPhone X', 'Apple', 3, 76000),
+       ('iPhone 8', 'Apple', 2, 51000),
+       ('Galaxy S9', 'Samsung', 2, 56000),
+       ('Galaxy S8', 'Samsung', 1, 41000),
+       ('P20 Pro', 'Huawei', 5, 36000);
+
+# 2
+select phone_name, manufacturer, price from phones
+where product_count > 2;
+
+# 3
+select * from phones
+where manufacturer = 'Samsung';
+
+# 4
+# (fast, 1 request)
+select *, (price * product_count) as total from phones
+where (price * product_count) between 100000 and 145000;
+# (slow, 2 request - select all rows)
+select * from (select *, (price * product_count) as total from phones) p
+where total between 100000 and 145000;
+# (fast, 2 request)
+select p.*, @total as total from phones p, (select @total := 0) t
+where id in (
+    select id from phones
+    where (@total := price * product_count) between 100000 and 145000
+    );
+
+select * from phones where phone_name like '%iphone%'; # 4.1
+select * from phones where phone_name like '%galaxy%'; # 4.2
+select * from phones where phone_name rlike '[0-9]+'; # 4.3
+select * from phones where phone_name rlike '8+'; # 4.4
---
+++ lesson1/work.sql
@@ -0,0 +1,30 @@
+# 1. Создание БД
+CREATE DATABASE IF NOT EXISTS gb_1; # Создать БД, если не существует
+# 2. Подключение
+USE gb_1; # Подключиться к БД
+# 3. Создание таблицы
+DROP TABLE IF EXISTS users; # Удалить таблицу, если существует
+CREATE TABLE # Создать таблицу
+users (
+	# имя тип_данных ограничения
+	`id` INT(16) PRIMARY KEY AUTO_INCREMENT,
+    `username` VARCHAR(32) UNIQUE NOT NULL COMMENT 'Имя пользователя (логин)',
+    `password` VARCHAR(64) NOT NULL COMMENT 'Хэш пароля',
+    `email` VARCHAR(64) NOT NULL COMMENT 'Эл. почта',
+    `phone` VARCHAR(11) NULL COMMENT 'Номер телефона',
+    `active` BOOLEAN NOT NULL DEFAULT TRUE COMMENT 'Активность'
+)
+AUTO_INCREMENT=0;
+# ALTER TABLE users AUTO_INCREMENT=100; # Изменение таблицы
+# 4. Добавление данных в таблицу
+INSERT INTO users(`username`, `password`, `email`, `phone`) # Добавление записи в таблицу
+VALUES
+	('admin', 'password_hash', 'admin@test.tt', ''),
+	('test', 'password_hash', 'test@test.tt', '9997770001'),
+    ('test2', 'password_hash', 'test2@test.tt', '9997770002');
+# 5. Обновление данных
+UPDATE users SET `password`='', `active`=false WHERE `username`='admin'; # Обновление записи в таблице
+# 6. Выборка
+# SELECT * FROM users LIMIT 10; # Вывод всех данных
+# SELECT username, email, phone FROM users WHERE username='test;
+SELECT* FROM users WHERE username LIKE 'test%';

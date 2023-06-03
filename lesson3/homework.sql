c552d154b14d78fb0d2d3040df3cfca344d8cb11
crasher307 <crasher307@gmail.com>
2023-06-03 08:17:58 +0500

    fix lesson 3

--- lesson3/homework.sql
+++ lesson3/homework.sql
@@ -1,19 +1,29 @@
 use gb_3;
 
 # 1
-select * from staff order by salary;
-select * from staff order by salary desc;
+select *
+from staff
+order by salary;
+select *
+from staff
+order by salary desc;
 
 # 2
-select * from staff order by salary desc limit 5;
+select *
+from staff
+order by salary desc
+limit 5;
 
 # 3
-select post, sum(salary) as 'sum' from staff
+select post, sum(salary) as 'sum'
+from staff
 group by post;
 
 # 4
 select count(id)
-from staff where post = 'Рабочий' and age between 24 and 49;
+from staff
+where post = 'Рабочий'
+  and age between 24 and 49;
 
 # 5
 select count(distinct post) as count
@@ -21,5 +31,6 @@ from staff;
 
 # 6
 select post, avg(age) as avg_age
-from staff group by post
+from staff
+group by post
 having avg_age < 30;
